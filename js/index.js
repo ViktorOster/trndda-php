a1449405ca7ab0a9301a7e0b88e8b946163256db
ViktorOster <Ozterkvlt@hotmail.com>
2019-11-13 19:24:12 +0200

    changed illustrations, fixed bugs

--- js/index.js
+++ js/index.js
@@ -55,25 +55,25 @@ const stepsMobile = document.querySelectorAll(".step-mobile")
 const stepsDesktop = document.querySelectorAll(".step")
 const dots = document.querySelectorAll(".dot")
 dots.forEach((dot, i) => {
-    dot.addEventListener("click", ()=> {
-        for(let j=0; j < dots.length; j++) {
-            if(j === i) {
+    dot.addEventListener("click", () => {
+        for (let j = 0; j < dots.length; j++) {
+            if (j === i) {
                 dots[j].classList += " is-active"
             } else {
                 dots[j].classList.remove("is-active")
             }
         }
 
-        for(let k=0; k < tutorialVideos.length; k++) {
-            if(k === i) {
+        for (let k = 0; k < tutorialVideos.length; k++) {
+            if (k === i) {
                 tutorialVideos[k].classList += " is-showing"
             } else {
                 tutorialVideos[k].classList.remove("is-showing")
             }
         }
 
-        for(let l=0; l < stepsMobile.length; l++) {
-            if(l === i) {
+        for (let l = 0; l < stepsMobile.length; l++) {
+            if (l === i) {
                 stepsMobile[l].classList += " is-showing"
             } else {
                 stepsMobile[l].classList.remove("is-showing")
@@ -84,24 +84,28 @@ dots.forEach((dot, i) => {
 
 stepsDesktop.forEach((step, i) => {
     step.addEventListener("mouseover", () => {
-        for(let j=0; j < dots.length; j++) {
-            if(j === i) {
+        stepsDesktop.forEach(stepToInActivate => {
+            stepToInActivate.classList.remove("is-active")
+        })
+        if (!step.classList.contains("is-active")) step.classList += " is-active"
+        for (let j = 0; j < dots.length; j++) {
+            if (j === i) {
                 dots[j].classList += " is-active"
             } else {
                 dots[j].classList.remove("is-active")
             }
         }
 
-        for(let k=0; k < tutorialVideos.length; k++) {
-            if(k === i) {
+        for (let k = 0; k < tutorialVideos.length; k++) {
+            if (k === i) {
                 tutorialVideos[k].classList += " is-showing"
             } else {
                 tutorialVideos[k].classList.remove("is-showing")
             }
         }
 
-        for(let l=0; l < stepsMobile.length; l++) {
-            if(l === i) {
+        for (let l = 0; l < stepsMobile.length; l++) {
+            if (l === i) {
                 stepsMobile[l].classList += " is-showing"
             } else {
                 stepsMobile[l].classList.remove("is-showing")
@@ -116,34 +120,47 @@ const modalSignInContent = document.querySelector(".modal__signIn")
 const signUpTabs = document.querySelectorAll(".modal__signUp-tab")
 const modalSignUpContent = document.querySelector(".modal__signUp")
 const darkOverlay = document.querySelector(".dark-overlay")
+const modalForgotPassword = document.querySelector(".modal__forgot-password")
+const forgotPasswordLink = document.querySelector("[data-js='forgot-password-link']")
 
 signInTabs.forEach(signInTab => {
     signInTab.addEventListener("click", () => {
-        if(!modalSignInContent.classList.contains("is-showing")) modalSignInContent.classList += " is-showing"
+        if (!modalSignInContent.classList.contains("is-showing")) modalSignInContent.classList += " is-showing"
         modalSignUpContent.classList.remove("is-showing")
         signInTabs.forEach(tab => {
-            if(!tab.classList.contains("is-active")) tab.classList += " is-active"
+            if (!tab.classList.contains("is-active")) tab.classList += " is-active"
         })
-        
+
         signUpTabs.forEach(signUpTab => {
             signUpTab.classList.remove("is-active")
         })
+        modalForgotPassword.classList.remove("is-showing")
     })
 })
 signUpTabs.forEach(signUpTab => {
     signUpTab.addEventListener("click", () => {
-        if(!modalSignUpContent.classList.contains("is-showing")) modalSignUpContent.classList += " is-showing"
+        if (!modalSignUpContent.classList.contains("is-showing")) modalSignUpContent.classList += " is-showing"
         modalSignInContent.classList.remove("is-showing")
         signUpTabs.forEach(tab => {
-            if(!tab.classList.contains("is-active")) tab.classList += " is-active"
+            if (!tab.classList.contains("is-active")) tab.classList += " is-active"
         })
-        
+
         signInTabs.forEach(signInTab => {
             signInTab.classList.remove("is-active")
         })
-
+        modalForgotPassword.classList.remove("is-showing")
     })
 })
+const modalTopContentMobileTabs = document.querySelector(".modal-top-content-mobile__tabs")
+forgotPasswordLink.addEventListener("click", () => {
+    if (!modalForgotPassword.classList.contains("is-showing")) modalForgotPassword.classList += " is-showing"
+    modalSignInContent.classList.remove("is-showing")
+    modalSignUpContent.classList.remove("is-showing")
+    signInTabs.forEach(signInTab => {
+        signInTab.classList.remove("is-active")
+    })
+})
+
 
 const modalCloseBtns = document.querySelectorAll(".modal__close")
 modalCloseBtns.forEach(modalClose => {
@@ -152,29 +169,36 @@ modalCloseBtns.forEach(modalClose => {
         darkOverlay.classList.remove("is-showing")
     })
 })
-
+const modalForgotPasswordBackBtn = document.querySelector(".modal-forgot-password__back")
+modalForgotPasswordBackBtn.addEventListener("click", () => {
+    modalForgotPassword.classList.remove("is-showing")
+    if (!modalSignInContent.classList.contains("is-showing")) modalSignInContent.classList += " is-showing"
+    signInTabs.forEach(tab => {
+        if (!tab.classList.contains("is-active")) tab.classList += " is-active"
+    })
+})
 
 const showModalButtons = document.querySelectorAll(".show-signIn-signUp-modal")
 showModalButtons.forEach(btn => {
     btn.addEventListener("click", () => {
-        if(!darkOverlay.classList.contains("is-showing")) darkOverlay.classList += " is-showing"
-        
-        if(!modalSignInSignUp.classList.contains("is-showing")) modalSignInSignUp.classList += " is-showing"
-  
-        if(btn.classList.contains("show-signIn")) {
-            if(!modalSignInContent.classList.contains("is-showing")) modalSignInContent.classList += " is-showing"
+        if (!darkOverlay.classList.contains("is-showing")) darkOverlay.classList += " is-showing"
+
+        if (!modalSignInSignUp.classList.contains("is-showing")) modalSignInSignUp.classList += " is-showing"
+
+        if (btn.classList.contains("show-signIn")) {
+            if (!modalSignInContent.classList.contains("is-showing")) modalSignInContent.classList += " is-showing"
             modalSignUpContent.classList.remove("is-showing")
             signInTabs.forEach(signInTab => {
-                if(!signInTab.classList.contains("is-active")) signInTab.classList += " is-active"
+                if (!signInTab.classList.contains("is-active")) signInTab.classList += " is-active"
             })
             signUpTabs.forEach(signUpTab => {
                 signUpTab.classList.remove("is-active")
             })
-        } else if(btn.classList.contains("show-signUp")) {
-            if(!modalSignUpContent.classList.contains("is-showing")) modalSignUpContent.classList += " is-showing"
+        } else if (btn.classList.contains("show-signUp")) {
+            if (!modalSignUpContent.classList.contains("is-showing")) modalSignUpContent.classList += " is-showing"
             modalSignInContent.classList.remove("is-showing")
             signUpTabs.forEach(signUpTab => {
-                if(!signUpTab.classList.contains("is-active")) signUpTab.classList += " is-active"
+                if (!signUpTab.classList.contains("is-active")) signUpTab.classList += " is-active"
             })
             signInTabs.forEach(signInTab => {
                 signInTab.classList.remove("is-active")
@@ -182,48 +206,49 @@ showModalButtons.forEach(btn => {
         }
 
     })
-    
-})
-const modalTutorialVideo = document.querySelector(".modal-tutorial-video")
-const showTutorialVideoModal = document.querySelector(".show-tutorial-video")
-const tutorialVideoClose = document.querySelector(".modal-tutorial-video__close")
 
-tutorialVideoClose.addEventListener("click", () => {
-    modalTutorialVideo.classList.remove("is-showing")
-    darkOverlay.classList.remove("is-showing")
 })
+// const modalTutorialVideo = document.querySelector(".modal-tutorial-video")
+// const showTutorialVideoModal = document.querySelector(".show-tutorial-video")
+// const tutorialVideoClose = document.querySelector(".modal-tutorial-video__close")
 
-showTutorialVideoModal.addEventListener("click", () => {
-    if(!darkOverlay.classList.contains("is-showing")) darkOverlay.classList += " is-showing"
-    
-    if(!modalTutorialVideo.classList.contains("is-showing")) modalTutorialVideo.classList += " is-showing"
-})
+// tutorialVideoClose.addEventListener("click", () => {
+//     modalTutorialVideo.classList.remove("is-showing")
+//     darkOverlay.classList.remove("is-showing")
+// })
+
+// showTutorialVideoModal.addEventListener("click", () => {
+//     if (!darkOverlay.classList.contains("is-showing")) darkOverlay.classList += " is-showing"
+
+//     if (!modalTutorialVideo.classList.contains("is-showing")) modalTutorialVideo.classList += " is-showing"
+// })
 
 const modalContainer = document.querySelector(".modal-container-2")
 modalContainer.addEventListener("click", (e) => {
     //close modal if clicked outside form 
     let clickedOnForm = false;
-    let clickedOnVideoModal = false;
+    // let clickedOnVideoModal = false;
     e.path.forEach(elem => {
-        if(elem.classList && elem.classList.contains("modal-signIn-signUp")) {
-          clickedOnForm = true;
-        } 
-        
+        if (elem.classList && elem.classList.contains("modal-signIn-signUp")) {
+            clickedOnForm = true;
+        }
+
     })
-    e.path.forEach(elem => {
-        if(elem.classList && elem.classList.contains("modal-tutorial-video")) {
-          clickedOnVideoModal = true;
-        } 
-        
-    })
-    if(!clickedOnForm) {
+    // e.path.forEach(elem => {
+    //     if (elem.classList && elem.classList.contains("modal-tutorial-video")) {
+    //         clickedOnVideoModal = true;
+    //     }
+
+    // })
+    if (!clickedOnForm) {
         modalSignInSignUp.classList.remove("is-showing")
         darkOverlay.classList.remove("is-showing")
+        modalForgotPassword.classList.remove("is-showing")
     }
-    if(!clickedOnVideoModal) {
-        modalTutorialVideo.classList.remove("is-showing")
-        darkOverlay.classList.remove("is-showing")
-    }
+    // if (!clickedOnVideoModal) {
+    //     modalTutorialVideo.classList.remove("is-showing")
+    //     darkOverlay.classList.remove("is-showing")
+    // }
 })
 function scrollToElement(yPos) {
     window.scrollTo({
@@ -240,17 +265,36 @@ const linksAbout = document.querySelectorAll("[data-js='about-link']")
 const linksContact = document.querySelectorAll("[data-js='contact-link']")
 linksTutorial.forEach(linkTutorial => {
     linkTutorial.addEventListener("click", () => {
-        scrollToElement(sectionTutorial.getBoundingClientRect().top)  
+        scrollToElement(sectionTutorial.offsetTop)
     })
 })
 linksAbout.forEach(linkAbout => {
     linkAbout.addEventListener("click", () => {
-        scrollToElement(sectionAbout.getBoundingClientRect().top)  
+        scrollToElement(sectionAbout.offsetTop)
     })
 })
 
 linksContact.forEach(linkContact => {
     linkContact.addEventListener("click", () => {
-        scrollToElement(sectionContact.getBoundingClientRect().top)  
+        scrollToElement(sectionContact.offsetTop)
     })
 })
+
+const fileExtensions = ["txt", "csv", "xls", "xlsx", "docx", "pdf", "sas", "spss", "stata", "htm", "html", "php", "asp"];
+let extensionIndex = 0;
+const extensionElement = document.querySelector(".file-extension")
+//used to occupy same space as the absolute extension element
+const extensionDummyElement = document.querySelector(".file-extension-dummy")
+
+const swapExtension = setInterval(function () {
+    extensionElement.classList.remove("swap-out")
+    extensionElement.classList += " swap-in"
+    extensionElement.textContent = fileExtensions[extensionIndex];
+    extensionDummyElement.textContent = fileExtensions[extensionIndex];
+    if (extensionIndex === fileExtensions.length - 1) extensionIndex = 0;
+    else extensionIndex++;
+    setTimeout(() => {
+        extensionElement.classList.remove("swap-in")
+        extensionElement.classList += " swap-out"
+    }, 1200);
+}, 1800);
